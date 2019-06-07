6149680662e3adcdd51cdcfe1561e337f6051dbb
Junfeng Li <lijunfen@amazon.com>
2019-06-07 14:44:16 -0700

    Unify import.

--- bin/hello_cdk.ts
+++ bin/hello_cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import cdk = require('@aws-cdk/cdk');
+import * as cdk from '@aws-cdk/cdk';
 import { HelloCdkStack } from '../lib/hello_cdk-stack';
 
 const app = new cdk.App();
--- lib/hello_cdk-stack.ts
+++ lib/hello_cdk-stack.ts
@@ -2,10 +2,12 @@ import * as cdk from '@aws-cdk/cdk';
 import * as s3 from '@aws-cdk/aws-s3';
 
 export class HelloCdkStack extends cdk.Stack {
+  bucket: s3.Bucket;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
+    this.bucket = new s3.Bucket(this, 'MyFirstBucket', {
       versioned: true,
       encryption: s3.BucketEncryption.KmsManaged,
     });
