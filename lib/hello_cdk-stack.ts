379512bc55ee461d74c33849a15552b1eda0f6fe
Junfeng Li <lijunfen@amazon.com>
2019-06-07 17:11:19 -0700

    Add CodeBuild.

--- lib/hello_cdk-stack.ts
+++ lib/hello_cdk-stack.ts
@@ -1,15 +1,19 @@
 import * as cdk from '@aws-cdk/cdk';
-import * as s3 from '@aws-cdk/aws-s3';
+import * as codebuild from '@aws-cdk/aws-codebuild';
 
 export class HelloCdkStack extends cdk.Stack {
-  bucket: s3.Bucket;
+  build: codebuild.Project;
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.bucket = new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      encryption: s3.BucketEncryption.KmsManaged,
+    this.build = new codebuild.Project(this, 'UnisonFSMonitor', {
+      source: new codebuild.GitHubSource({
+        owner: 'autozimu',
+        repo: 'unison-fsmonitor',
+        webhook: true,
+        webhookFilters: [],
+      }),
     });
   }
 }
